3bcca442844f2eb5ea1976a3d4f0e73c837020c7
Ozraam <lhgrillepain@gmail.com>
2023-02-14 23:36:34 +0100

    Client on connect sending work

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import { Player } from "./game/Player.js"
+
 const app = express();
-const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+
+
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:5173',
   }
 });
 
-app.use(express.static(__dirname+"/public"))
+const rooms = new Map();
+
+//app.use(express.static(__dirname+"/public"))
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -20,11 +26,27 @@ io.on('connection', (socket) => {
   })
 
   socket.on('joinRoom', (data) => {
-    console.log('joinRoom', data)
-    socket.join(data.room)
+    console.log(rooms, rooms.has(data.roomNumber));
+    socket.join(data.roomNumber)
     username = data.username
+
+    if (rooms.has(data.roomNumber)) {
+      rooms.get(data.roomNumber).push(new Player(data.username))
+    } else {
+      rooms.set(data.roomNumber, [new Player(data.username)])
+    }
+
+    sendData(data.roomNumber, rooms.get(data.roomNumber), socket, 'getPlayers')
   });
 });
+
+function sendData(room, data, socket, event, time = 500) {
+  
+  setTimeout(() => {
+    console.log("sending data");
+    io.to(room).emit(event, data);
+  }, time);
+}
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
